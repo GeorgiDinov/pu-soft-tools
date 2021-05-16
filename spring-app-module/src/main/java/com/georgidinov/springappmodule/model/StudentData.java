65866a7f4d6bddc59e92665875347fc098d878cb
Georgi Dinov <georgidinov1@abv.bg>
2021-05-16 09:13:37 +0300

    Added model representing the return JSON from the end point

---
+++ spring-app-module/src/main/java/com/georgidinov/springappmodule/model/StudentData.java
@@ -0,0 +1,50 @@
+package com.georgidinov.modulea.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import java.io.Serializable;
+
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class StudentData implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @JsonProperty("student_name")
+    private String studentName;
+    @JsonProperty("faculty_number")
+    private String facultyNumber;
+
+    public StudentData() {
+    }
+
+    public StudentData(String studentName, String facultyNumber) {
+        this.studentName = studentName;
+        this.facultyNumber = facultyNumber;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getFacultyNumber() {
+        return facultyNumber;
+    }
+
+    public void setFacultyNumber(String facultyNumber) {
+        this.facultyNumber = facultyNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentData{" +
+                "studentName='" + studentName + '\'' +
+                ", facultyNumber='" + facultyNumber + '\'' +
+                '}';
+    }
+}
