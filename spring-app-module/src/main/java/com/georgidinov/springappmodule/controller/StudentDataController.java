85042dd3c70ea6e731f54607ffee9323420b1745
Georgi Dinov <georgidinov1@abv.bg>
2021-05-16 09:15:12 +0300

    Added controller to return predefined(hard coded) model representation on base application url

---
+++ spring-app-module/src/main/java/com/georgidinov/springappmodule/controller/StudentDataController.java
@@ -0,0 +1,26 @@
+package com.georgidinov.modulea.controller;
+
+import com.georgidinov.modulea.model.StudentData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentDataController {
+
+    Logger log = LoggerFactory.getLogger(StudentDataController.class);
+
+    @GetMapping("/")
+    @ResponseStatus(HttpStatus.OK)
+    public StudentData studentData() {
+        StudentData dinovData = new StudentData();
+        dinovData.setStudentName("Georgi Dinov");
+        dinovData.setFacultyNumber("2001717005");
+        log.info("Sending student data {}", dinovData);
+        return dinovData;
+    }
+
+}
